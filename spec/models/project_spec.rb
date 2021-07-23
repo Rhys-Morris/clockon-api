6b802c4500847b7756e2a73ae9a0d5ba644097bb
Rhys Morris <rhysmorris08@gmail.com>
2021-07-23 12:08:35 +1000

    Project validation specs

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @project = build :project
+  end
+
+  context "validations" do
+    it "should have a name" do
+      @project.name = "    "
+      expect(@project).to be_invalid
+      @project.name = nil
+      expect(@project).to be_invalid
+    end  
+
+    it "should not have a name > 40 chars" do
+      @project.name = "a" * 41
+      expect(@project).to be_invalid
+    end
+
+    it "should have a boolean active attribute" do
+      @project.active = nil
+      expect(@project).to be_invalid
+    end
+
+    it "should accept booleans for active attribute" do
+      @project.active = false
+      expect(@project).to be_valid
+    end
+
+    it "should have a boolean billable attribute" do
+      @project.billable = nil
+      expect(@project).to be_invalid
+    end
+
+    it "should accept booleans for billable attribute" do
+      @project.billable = false
+      expect(@project).to be_valid
+    end
+
+    it "should have a due_date property that is an instance of the Date class" do
+      expect(@project.due_date).to be_instance_of Date
+      @project.due_date = nil
+      expect(@project).to be_invalid
+    end
+  end
+
 end
