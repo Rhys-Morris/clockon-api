86cfafef90b09af3f3526a99b9a8953caeec274f
Rhys Morris <rhysmorris08@gmail.com>
2021-07-28 13:15:56 +1000

    Set up WorkPeriod controller and routes

---
+++ app/controllers/work_periods_controller.rb
@@ -0,0 +1,49 @@
+class WorkPeriodsController < ApplicationController
+  before_action :set_work_period, only: [:update, :destroy]
+  before_action :set_project, only: [:create, :update, :destroy]
+  before_action :fetch_all_work_periods, only: [:index]
+  
+  def index
+    render json: { work_periods: @work_periods }
+  end
+
+  def create
+      @work_period = @project.work_periods.create(work_period_params)
+      if @work_period.valid?
+          render json: { work_periods: fetch_all_work_periods }, status: 201
+      else
+          render json: { error: @work_period.errors.full_messages }
+      end    
+  end
+
+  def update
+      if @work_period.update(work_period_params)
+          render json: { work_periods: fetch_all_work_periods }, status: 200
+      else
+          render json: { error: @task.errors.full_messages }
+      end   
+  end
+
+  def destroy
+      @work_period.destroy
+      render json: { work_periods: fetch_all_work_periods }, status: 200
+  end
+
+  private
+
+  def set_work_period
+      @work_period = WorkPeriod.find(params[:id])
+  end
+
+  def work_period_params
+      params.require(:work_period).permit(:id, :start_time, :end_time, :title, :project_id)
+  end
+
+  def set_project
+      @project = @user.projects.find(params[:project_id])
+  end
+
+  def fetch_all_work_periods
+    @work_periods = WorkPeriod.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'work_periods/index'
+  get 'work_periods/create'
+  get 'work_periods/update'
+  get 'work_periods/destroy'
   # LOGIN & AUTH
   post "/register", to: "users#create"
   post "/login", to: "users#login"
@@ -14,13 +18,16 @@ Rails.application.routes.draw do
   put "/projects/:id", to: "projects#update"
   delete "/projects/:id", to: "projects#destroy"
   # TASKS
-  get "/tasks/:project_id", to: "tasks#index"
   post "/tasks/:project_id", to: "tasks#create"
   put "/tasks/:project_id/:id", to: "tasks#update"
   delete "/tasks/:project_id/:id", to: "tasks#destroy"
   # EXPENSES
-  get "/expenses/:project_id", to: "expenses#index"
   post "/expenses/:project_id", to: "expenses#create"
   put "/expenses/:project_id/:id", to: "expenses#update"
   delete "/expenses/:project_id/:id", to: "expenses#destroy"
+  # WORK PERIODS
+  get "/work", to: "work_periods#index"
+  post "/work/:project_id", to: "work_periods#create"
+  put "/work//:project_id/:id", to: "work_periods#update"
+  delete "/work/:project_id/:id", to: "work_periods#destroy"
 end
